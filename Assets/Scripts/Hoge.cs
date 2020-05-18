a507f50df8eeb3bb2e4866889d899614c45a9f99
segur <segur.opus@gmail.com>
2020-05-18 12:30:00 +0900

    Added Hoge.cs

---
+++ Assets/Scripts/Hoge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Hoge : ScriptableObject
+{
+    [SerializeField]
+    private int intValue;
+
+    public int IntValue
+    {
+        get { return intValue; }
+#if UNITY_EDITOR
+        set { intValue = Mathf.Clamp(value, 0, int.MaxValue); }
+#endif
+    }
+}
