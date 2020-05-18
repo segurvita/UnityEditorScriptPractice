72a61317619bf5f79a8197f168428bdbd7560901
segur <segur.opus@gmail.com>
2020-05-18 12:30:50 +0900

    Added HogeLoader.cs

---
+++ Assets/Scripts/HogeLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HogeLoader : MonoBehaviour
+{
+    private Hoge m_hoge;
+
+    private void Start()
+    {
+        m_hoge = ScriptableObject.Instantiate(Resources.Load<Hoge>("Hoge"));
+
+        Debug.Log(m_hoge.IntValue);
+    }
+}
